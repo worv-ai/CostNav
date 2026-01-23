0173f6aa868f9d76369a0b86316b88f207f904d5
Haebin Seong <hbseong97@gmail.com>
2026-01-23 22:49:27 +0900

    add EpisodeTerm_PhysicalAssistance

--- scripts/eval_nav2.sh
+++ scripts/eval_nav2.sh
@@ -179,10 +179,10 @@ run_mission() {
                 mission_result="SUCCESS"
                 log "Mission $mission_num: Goal reached! Distance: ${distance_to_goal}m"
                 break
-            elif [ "$result_status" = "failure" ]; then
+            elif [[ "$result_status" == failure_* ]]; then
                 mission_result="FAILED"
-                error_msg="Timeout - distance to goal: ${distance_to_goal}m"
-                log "Mission $mission_num: Timeout! Distance: ${distance_to_goal}m"
+                error_msg="${result_status} - distance to goal: ${distance_to_goal}m"
+                log "Mission $mission_num: ${result_status}! Distance: ${distance_to_goal}m"
                 break
             fi
 
